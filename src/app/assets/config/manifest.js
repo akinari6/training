eb4421b2c32b6240f6233c8fc7f3896f61162ae0
atsuji <ttakinai@yahoo.co.jp>
2025-11-07 02:47:44 +0900

    ローカルで動作まで

--- src/app/assets/config/manifest.js
+++ src/app/assets/config/manifest.js
@@ -1,2 +1,8 @@
+// app/assets/config/manifest.js
 //= link_tree ../images
 //= link_directory ../stylesheets .css
+//= link_tree ../builds
+//= link application.css
+//= link application.js
+//= link controllers/hello_controller.js
+//= link controllers/index.js
